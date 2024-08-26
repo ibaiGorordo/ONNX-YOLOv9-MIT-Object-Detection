7718c9af2d3662a6d52d523448b4b2cfce0fa96b
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-26 10:19:24 +0900

    Update video_object_detection.py

--- video_object_detection.py
+++ video_object_detection.py
@@ -1,20 +1,16 @@
+from datetime import timedelta
 import cv2
 from cap_from_youtube import cap_from_youtube
 
 from yolov9 import YOLOv9, draw_detections
 
-# # Initialize video
-# cap = cv2.VideoCapture("video.avi")
+# Initialize video
 videoUrl = 'https://youtu.be/hmk5cxpAfcw?si=hDOibrI22grM8Aqa'
-cap = cap_from_youtube(videoUrl)
-start_time = 4*60 # skip first {start_time} seconds
-fps = cap.get(cv2.CAP_PROP_FPS)
-cap_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-cap_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-cap.set(cv2.CAP_PROP_POS_FRAMES, start_time * fps)
+start_time = timedelta(minutes=4, seconds=0)
+cap = cap_from_youtube(videoUrl, start=start_time)
 
 # Initialize object detector
-model_path = "models/v9-c_mit.onnx"
+model_path = "models/v9-m_mit.onnx"
 detector = YOLOv9(model_path, conf_thres=0.3)
 
 cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
